98a671867665a0347d5e9f21eef2fd607f1695a0
dmorenog01 <diegomoreno102012@gmail.com>
2022-07-31 23:49:33 -0400

    Added BasicAuth to the API.

--- api.py
+++ api.py
@@ -1,10 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_httpauth import HTTPBasicAuth
 import requests
 from bs4 import BeautifulSoup
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 URL = "http://www.bcv.org.ve/estadisticas/tipo-cambio-de-referencia-smc"
+
+USERS = {
+    "TasaVE": 'pbkdf2:sha256:260000$uDAaEwXpgfBSMe6P$e8af0093fd2608e349c5a8a74b0c2d0ef01f08601e43a63b8eeac195e1bfbb1c'
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    # Autenticates users in the API
+    if username in USERS and check_password_hash(USERS.get(username), password):
+        return username
+
+@auth.error_handler
+def auth_error(status):
+    # Returns an empty response in case of 
+    return {"rate": 0, "rate-date": None, "date-acquired": None, "hour-acquired": None, "code": status}, status
 
 def BCVRate():
     try:
@@ -25,6 +43,7 @@ def BCVRate():
     return {"rate": tasa, "rate-date": date, "date-acquired": date_acquired, "hour-acquired": hour_acquired, "code": 200}
 
 @app.route("/", methods=["GET"])
+@auth.login_required
 def index():
     return BCVRate()
 
